60c4a46df656de8db1b64940e723b8aaf964def4
kusma007 <kusma007@inbox.ru>
2016-02-05 19:05:04 +0200

    fix layouts

--- themes/bistro/layouts/main.php
+++ themes/bistro/layouts/main.php
@@ -23,53 +23,53 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-<!---->
+
 <!--<div class="wrap">-->
-<!--    --><?php
-//    NavBar::begin([
-//        'brandLabel' => 'My Company',
-//        'brandUrl' => Yii::$app->homeUrl,
-//        'options' => [
-//            'class' => 'navbar-inverse navbar-fixed-top',
-//        ],
-//    ]);
-//    echo Nav::widget([
-//        'options' => ['class' => 'navbar-nav navbar-right'],
-//        'items' => [
-//            ['label' => 'Home', 'url' => ['/site/index']],
-//            ['label' => 'About', 'url' => ['/site/about']],
-//            ['label' => 'Contact', 'url' => ['/site/contact']],
-//            Yii::$app->user->isGuest ?
-//                ['label' => 'Login', 'url' => ['/site/login']] :
-//                [
-//                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-//                    'url' => ['/site/logout'],
-//                    'linkOptions' => ['data-method' => 'post']
-//                ],
-//        ],
-//    ]);
-//    NavBar::end();
-//    ?>
-<!---->
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'My Company',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Contact', 'url' => ['/site/contact']],
+            Yii::$app->user->isGuest ?
+                ['label' => 'Login', 'url' => ['/site/login']] :
+                [
+                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                    'url' => ['/site/logout'],
+                    'linkOptions' => ['data-method' => 'post']
+                ],
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
 <!--    <div class="container">-->
 <!--        width="760" height="500"-->
-        <iframe style="width:100%; height: 2050px" src="https://docs.google.com/forms/d/1JVI1V1blssyq6GmJUR5nhrO7Q1AeAKSfrzBNjM40lfs/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe>
-<!---->
+
 <!--        --><?php //echo Breadcrumbs::widget([
 //            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 //        ]) ?>
-<!--        --><?php // echo $content ?>
+<div style="padding-top: 50px"></div>
+        <?php  echo $content ?>
 <!--    </div>-->
 <!--</div>-->
-<!---->
-<!--<footer class="footer">-->
-<!--    <div class="container">-->
-<!--        <p class="pull-left">&copy; My Company --><?//= date('Y') ?><!--</p>-->
-<!---->
-<!--        <p class="pull-right">--><?//= Yii::powered() ?><!--</p>-->
-<!--    </div>-->
-<!--</footer>-->
-<!---->
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
 <?php $this->endBody() ?>
 </body>
 </html>
--- themes/bistro/site/index.php
+++ themes/bistro/site/index.php
@@ -1,9 +1,1 @@
-<?php
-//
-///* @var $this yii\web\View */
-//
-//$this->title = 'My Yii Application';
-//?>
-<!--<div class="site-index">-->
-
-<!--    <iframe src="https://docs.google.com/forms/d/1JVI1V1blssyq6GmJUR5nhrO7Q1AeAKSfrzBNjM40lfs/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe>-->
+<iframe style="width:100%; height: 2050px" src="https://docs.google.com/forms/d/1JVI1V1blssyq6GmJUR5nhrO7Q1AeAKSfrzBNjM40lfs/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe>
