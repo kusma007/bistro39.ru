971ffb76e34ec2443bd97fdc267437958c097738
kusma007 <kusma007@inbox.ru>
2016-02-07 14:36:50 +0200

    fix layout

--- themes/bistro/layouts/main.php
+++ themes/bistro/layouts/main.php
@@ -39,8 +39,9 @@ $controller_action = Yii::$app->controller->route;
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
+                    ['label' => 'Content', 'url' => ['/content-menu/index']],
+//                    ['label' => 'About', 'url' => ['/site/about']],
+//                    ['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
                         [
