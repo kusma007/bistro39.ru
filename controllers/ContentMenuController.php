ad5d56478ddcd4961c23fbcb72430558c996d4f9
kusma007 <kusma007@inbox.ru>
2016-02-09 00:32:38 +0200

    add function for cron update status

--- controllers/ContentMenuController.php
+++ controllers/ContentMenuController.php
@@ -126,7 +126,23 @@ class ContentMenuController extends Controller
         } else {
             return 'Error';
         }
+    }
 
+    //bistro39.ru/content-menu/update-status
+    public function actionUpdateStatus()
+    {
+        $model_on = ContentMenu::findOne(['status' => 'on']);
+        $model_on->status = 'off';
+        $id_off = $model_on->id+1;
+
+        $count_content = ContentMenu::find()->count();
+        if($id_off > $count_content) {$id_off = 1;}
+        $model = $this->findModel($id_off);
+        $model->status = 'on';
+
+        if($model->save() && $model_on->save()) {
+            return true;
+        }
     }
 
     /**
